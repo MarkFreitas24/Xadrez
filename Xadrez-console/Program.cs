809665a0cdcde7d64587c9ccb719b5388017c47f
Mark Freitas <markfreitas24@gmail.com>
2023-12-30 11:42:48 +0000

    Criando a classe posicao xadrez

--- Xadrez-console/Program.cs
+++ Xadrez-console/Program.cs
@@ -7,20 +7,10 @@ namespace Xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board gameBoard = new Board(8, 8);
+            ChessPosition pos = new ChessPosition('c', 7);
 
-                gameBoard.PlacePart(new Tower(gameBoard, Color.Preta), new Position(0, 0));
-                gameBoard.PlacePart(new Tower(gameBoard, Color.Preta), new Position(1, 3));
-                gameBoard.PlacePart(new King(gameBoard, Color.Preta), new Position(2, 4));
-
-                Screen.PrintBoard(gameBoard);
-            }
-            catch(BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosition());
 
             Console.WriteLine();
         }
